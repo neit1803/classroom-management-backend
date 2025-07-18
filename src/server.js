2aabc3e26ff0ff42fbdaeec42c7455dd29b041ae
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-07-18 16:52:50 +0700

    Update: apply Instructor & Student routers

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const instrutorRoutes = require('./routes/instructorRoutes');
 
-require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 8888;
+const host_name = process.env.HOST_NAME || 'localhost';
 
-const port = process.env.PORT || 8888
-const host_name = process.env.HOST_NAME || 'localhost'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Define Routes
+app.use("/", instrutorRoutes);
 
 app.listen(port, host_name, () => {
   console.log(`Example app running on ${host_name}:${port}/`)
-})
+});
