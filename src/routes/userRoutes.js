c9591c37e4c48a4b072a48445449d3e2224f4df1
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-07-20 12:40:12 +0700

    Initialize user Router

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const userRouter = express.Router();
+
+userRouter.use(express.json());
+userRouter.use(express.urlencoded({ extended: true }));
+
+// GET requests
+userRouter.get('/myLessons', userController.getStudentLessons);
+userRouter.get('/students', userController.getAllStudents);
+userRouter.get('/student/:phone', userController.getStudentByPhone);
+
+// POST requests
+userRouter.post('/markLessonDone', userController.markLessonDone);
+userRouter.post('/addStudent', userController.addStudent);
+userRouter.post('/assignLesson', userController.addLesson);
+
+// PUT requests
+userRouter.put('/editProfile', userController.editProfile);
+userRouter.put('/editStudent/:phone', userController.editProfile);
+
+// DELETE requests
+userRouter.delete('/deleteStudent/:phone', userController.deleteStudent);
+
+module.exports = userRouter;
