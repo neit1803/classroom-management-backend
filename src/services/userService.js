3c90ba7addc1c519f8d38b51a31ec486ed8106f6
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-07-22 11:53:13 +0700

    Create User Service

---
+++ src/services/userService.js
@@ -0,0 +1,53 @@
+const { User } = require('../models/User');
+const firebaseService = require('../services/firebaseService');
+
+const collection = 'users';
+
+const userService = {
+    // READ requests
+    getAllStudents: async () => {
+        return await firebaseService.getAllDocuments(collection);
+    },
+
+    getStudentByPhone: async (phone) => {
+        return await firebaseService.queryByField(collection, 'phone', phone);
+    },
+
+    getStudentByEmail: async (email) => {
+        return await firebaseService.queryByField(collection, 'email', email);
+    },
+
+    // CREATE requests
+    addUser: async (studentData) => {
+        // if (studentData.email.contains('@') === false || studentData.email.includes(' ')) {
+        //     return 'Invalid email format';
+        // }
+        if (studentData.phone.length < 10 || studentData.phone.length > 15 || !/^\d+$/.test(studentData.phone)) {
+            return 'Invalid phone number format';
+        }
+        if (!studentData.email || !studentData.phone) {
+            return 'Email and phone are required to create a user';
+        }
+        if (await userService.getStudentByEmail(studentData.email).then(existingUser => existingUser.length > 0)) {
+            console.log(userService.getStudentByEmail(studentData.email));
+            return 'User with this email already exists';
+        }
+        if (await userService.getStudentByPhone(studentData.phone).then(existingUser => existingUser.length > 0)) {
+            return 'User with this phone number already exists';
+        }
+        return await firebaseService.addDocument(collection, studentData);
+    },
+
+    // UPDATE requests
+    updateStudent: async (phone, studentData) => {
+        const user = new User(studentData);
+        return await firebaseService.updateDocument(collection, phone, user);
+    },
+
+    // DELETE requests
+    deleteUser: async (phone) => {
+        return await firebaseService.deleteDocumentByPhone(collection, phone);
+    }
+}
+
+module.exports = userService;
