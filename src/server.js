6ef3f7148afacecc68819f00675737e95496bd33
neit1803 <106059540+neit1803@users.noreply.github.com>
2025-07-29 17:20:11 +0700

    Connect Lesson Router

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
+const lessonRoutes = require('./routes/lessonRoutes');
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -9,6 +10,7 @@ const host_name = process.env.HOST_NAME || 'localhost';
 
 // Define Routes
 app.use("/", userRoutes);
+app.use("/", lessonRoutes);
 
 app.listen(port, host_name, () => {
   console.log(`Example app running on ${host_name}:${port}/`)
